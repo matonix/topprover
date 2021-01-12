a44ed2adfae808ca8004962e2bfd94f4095f0069
matonix <automatonix@gmail.com>
2021-01-13 02:04:15 +0900

    update 5

--- 5/Example.v
+++ 5/Example.v
@@ -1,7 +1,11 @@
-Require Import Problem.
+Require Import Problem Coq.Arith.Mult Coq.Arith.Plus.
 
 Theorem solution: task.
 Proof.
   unfold task.
-  (* FILL IN HERE *)
+  intros.
+  assert (S m = 1 + m).
+  - reflexivity.
+  - rewrite H. rewrite mult_plus_distr_l. rewrite mult_1_r. reflexivity.
 Qed.
+
